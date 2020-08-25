0e01c253c7ecf62a6e206c59ccf80306351ad8dc
Nazar Kulchytskyi <nazarkin124@gmail.com>
2020-08-25 16:51:37 +0300

    init

---
+++ TestMonitor/Monitor.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+using NUnit.Framework;
+
+namespace TestMonitor
+{
+    public class Monitor
+    {
+        [Extension]
+        public class MyEventListener : ITestEventListener
+        {
+            public void OnTestEvent(string report)
+            {
+                TestContext.Out.WriteLine("Hello world 2" + report);
+            }
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            TestContext.Out.WriteLine("Hello world 1");
+            Assert.Fail();
+        }
+    }
+}
